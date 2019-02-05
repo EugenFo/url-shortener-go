c844ff6e537e9f070ac6055c4a6e45befc57ba40
EugenFo <eugen.fohlenweider@hotmail.com>
2019-02-05 10:59:54 +0100

    add auto increment for id, redirect search with mongodb

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/sha1"
 	"encoding/hex"
 	"encoding/json"
@@ -11,12 +12,15 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/mongo"
+
 	"github.com/gorilla/mux"
 )
 
 // mock data Struct
 type jsonData struct {
-	ID         int       `json:"id"`
+	ID         int       `json:"_id"`
 	LongURL    string    `json:"longurl"`
 	ShortURL   string    `json:"shorturl"`
 	CreateDate time.Time `json:"createDate"`
@@ -44,6 +48,24 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	lastID := mockData[len(mockData)-1].ID + 1
 	fmt.Println(strconv.Itoa(lastID))
 
+	// connect to Database
+	mongoIP := "mongodb://127.0.0.1:27017"
+	ctx, cancle := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, mongoIP)
+	if err != nil {
+		fmt.Println("MongoDB connection has an error:", err)
+	}
+	defer cancle()
+	saveCollection := client.Database("testdb").Collection("testCollection")
+
+	// count Documents in the Collection for auto increment of the id
+	res, err := saveCollection.CountDocuments(ctx, bson.M{})
+	if err != nil {
+		fmt.Println("error while counting the Documents:", err)
+	}
+	fmt.Println("result of CountDocuments:", res)
+	res++
+
 	// using unix timestamp for hashing
 	timeNow := time.Now()
 	timeUnix := timeNow.Unix()
@@ -57,13 +79,37 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(shortURL)
 
 	mockData = append(mockData, jsonData{ID: lastID, LongURL: parsedData, ShortURL: shortURL, CreateDate: timeNow})
+
+	// save to database
+	res2, err := saveCollection.InsertOne(ctx, bson.M{"ID": int(res), "LongURL": parsedData, "ShortURL": shortURL, "CreateDate": timeNow})
+	fmt.Println("inserted Doc:", res2.InsertedID)
+	if err != nil {
+		fmt.Println("save error:", err)
+	}
 }
 
 func redirectPage(w http.ResponseWriter, r *http.Request) {
 	// search in MongoDDB for the given id in the URL and redirect to the longurl behind the id
 	// atm it redirects to the url which is typed after the slash
 	params := mux.Vars(r)
-	http.Redirect(w, r, "https://"+params["id"], http.StatusMovedPermanently)
+	mongoIP := "mongodb://127.0.0.1:27017"
+	ctx, cancle := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, mongoIP)
+	if err != nil {
+		fmt.Println("MongoDB connection has an error:", err)
+	}
+	defer cancle()
+	getCollection := client.Database("testdb").Collection("testCollection")
+
+	fmt.Println("get params for redir:", params["id"])
+	filter := bson.M{"ShortURL": params["id"]}
+	var result bson.M
+	err = getCollection.FindOne(ctx, filter).Decode(&result)
+	if err != nil {
+		fmt.Println("Error while findOne:", err)
+	}
+	long := result["LongURL"]
+	http.Redirect(w, r, long.(string), http.StatusMovedPermanently)
 }
 
 func main() {
@@ -72,6 +118,14 @@ func main() {
 	now := time.Now()
 	mockData = append(mockData, jsonData{ID: 1, LongURL: "google.com", ShortURL: "random!", CreateDate: now})
 	mockData = append(mockData, jsonData{ID: 2, LongURL: "twitter.com", ShortURL: "random!2", CreateDate: now})
+
+	// create MongoDB connection
+	/* mongoIP := "mongodb://localhost:27017"
+	ctx, cancle := context.WithTimeout(context.Background(), 2*time.Second)
+	client, err := mongo.Connect(ctx, mongoIP)
+	if err != nil {
+		fmt.Println("MongoDB connection has an error:", err)
+	} */
 
 	// Route Handler
 	r.HandleFunc("/", mainPage).Methods("GET")
