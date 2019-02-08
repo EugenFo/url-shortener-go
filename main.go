d29ad48305571e99d33bc564a27c49700909813f
EugenFo <eugen.fohlenweider@hotmail.com>
2019-02-08 10:19:12 +0100

    mongodb implementation complete

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/sha1"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -17,55 +16,16 @@ import (
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
-type jsonData struct {
-	ID json.Number `json:"ID,Number"`
-	//	_ID        string `json:"_id"`
-	LongURL  string `json:"LongURL"`
-	ShortURL string `json:"ShortURL"`
-	//CreateDate time.Time `json:"CreateDate"`
+type shortenerData struct {
+	ID         int
+	ShortURL   string
+	LongURL    string
+	CreateDate time.Time
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("static/index.html")
 	tmpl.Execute(w, nil)
-}
-
-// func to get all the item in the slice. Just for debug purpose
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	mongoIP := "mongodb://127.0.0.1:27017"
-	ctx, cancle := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, mongoIP)
-	if err != nil {
-		fmt.Println("MongoDB connection has an error:", err)
-	}
-	defer cancle()
-	newCollection := client.Database("testdb").Collection("testCollection")
-
-	// cursor to iterate through the found documents
-	cur, err := newCollection.Find(ctx, bson.M{})
-	if err != nil {
-		fmt.Println("error while fetching all data", err)
-	}
-	defer cur.Close(ctx)
-	var rst bson.M
-	var p jsonData
-	for cur.Next(ctx) {
-		err := cur.Decode(&rst)
-		if err != nil {
-			fmt.Println("error while decoding:", err)
-		}
-
-		res1, _ := json.Marshal(rst)
-
-		err = json.Unmarshal(res1, &p)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-		json.NewEncoder(w).Encode(p)
-	}
-
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
@@ -86,23 +46,18 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("error while counting the Documents:", err)
 	}
-	fmt.Println("result of CountDocuments:", res)
 	res++
 
 	// using unix timestamp for hashing
 	timeNow := time.Now()
 	timeUnix := timeNow.Unix()
 
-	// at first I'm gonna use SHA1 and only the first 4 bits of the hash
-	// need to use bijectiv and base62 encoding. best practice
-
 	// converts int64 into string and creates an SHA1 hash
 	hash := sha1.Sum([]byte(strconv.FormatInt(timeUnix, 10)))
 	shortURL := hex.EncodeToString(hash[:3])
 
 	// save to database
-	res2, err := saveCollection.InsertOne(ctx, bson.M{"ID": int(res), "LongURL": parsedData, "ShortURL": shortURL, "CreateDate": timeNow})
-	fmt.Println("inserted Doc:", res2.InsertedID)
+	_, err = saveCollection.InsertOne(ctx, shortenerData{ID: int(res), ShortURL: shortURL, LongURL: parsedData, CreateDate: timeNow})
 	if err != nil {
 		fmt.Println("save error:", err)
 	}
@@ -110,8 +65,12 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 
 func redirectPage(w http.ResponseWriter, r *http.Request) {
 	// search in MongoDDB for the given id in the URL and redirect to the longurl behind the id
-	// atm it redirects to the url which is typed after the slash
 	params := mux.Vars(r)
+
+	// because of double GET request...
+	if params["id"] == "favicon.ico" || params["id"] == "" {
+		return
+	}
 	mongoIP := "mongodb://127.0.0.1:27017"
 	ctx, cancle := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, mongoIP)
@@ -121,31 +80,22 @@ func redirectPage(w http.ResponseWriter, r *http.Request) {
 	defer cancle()
 	getCollection := client.Database("testdb").Collection("testCollection")
 
-	fmt.Println("get params for redir:", params["id"])
-	filter := bson.M{"ShortURL": params["id"]}
-	var result bson.M
+	filter := bson.D{{"shorturl", params["id"]}}
+	var result shortenerData
 	err = getCollection.FindOne(ctx, filter).Decode(&result)
 	if err != nil {
 		fmt.Println("Error while findOne:", err)
 	}
-	long := result["LongURL"]
-	http.Redirect(w, r, long.(string), http.StatusMovedPermanently)
+	http.Redirect(w, r, result.LongURL, http.StatusMovedPermanently)
+
 }
 
 func main() {
 	r := mux.NewRouter()
-	// create MongoDB connection
-	/* mongoIP := "mongodb://localhost:27017"
-	ctx, cancle := context.WithTimeout(context.Background(), 2*time.Second)
-	client, err := mongo.Connect(ctx, mongoIP)
-	if err != nil {
-		fmt.Println("MongoDB connection has an error:", err)
-	} */
 
 	// Route Handler
 	r.HandleFunc("/", mainPage).Methods("GET")
 	r.HandleFunc("/{id}", redirectPage).Methods("GET")
-	r.HandleFunc("/get/", getHandler).Methods("GET")
 	r.HandleFunc("/save/", saveHandler).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
